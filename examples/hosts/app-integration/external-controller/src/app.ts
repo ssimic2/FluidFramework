05f999486fe894dc17ed68de1f082617b57ca151
Sasha Simic <sashasimic@microsoft.com>
2022-03-23 14:47:20 -0700

    Module export fix.

--- examples/hosts/app-integration/external-controller/src/app.ts
+++ examples/hosts/app-integration/external-controller/src/app.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
+import { ContainerDebugLogger } from "@fluid-example/container-debugger";
 import {
     AzureFunctionTokenProvider,
     AzureClient,
@@ -79,7 +80,7 @@ async function start(): Promise<void> {
     // and hook to the Telemetry system
     const clientProps = {
         connection: connectionConfig,
-        // logger: new ContainerDebugLogger(),
+        logger: new ContainerDebugLogger(),
     };
     const client = new AzureClient(clientProps);
     let container: IFluidContainer;
