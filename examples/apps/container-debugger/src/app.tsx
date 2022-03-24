a621571bcb53f3346d751b602b4a2a1d6426cabc
Sasha Simic <sashasimic@microsoft.com>
2022-03-23 22:21:52 -0700

    Displaying/filtering container telemetry.

--- examples/apps/container-debugger/src/app.tsx
+++ examples/apps/container-debugger/src/app.tsx
@@ -5,29 +5,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { AzureClient } from "@fluidframework/azure-client";
-
-import { IFluidContainer, SharedMap } from "fluid-framework";
-import { connectionConfig } from "./azureConfig";
-
 import { AppView } from "./appView";
-import { EventItemType } from "./dataTypes";
-// Define the schema of our Container.
-const containerSchema = {
-    initialObjects: {
-        dataMap: SharedMap,
-        recoveryMap: SharedMap,
-    },
-};
 
 async function start(): Promise<void> {
-    const clientProps = {
-        connection: connectionConfig,
-    };
-    const client = new AzureClient(clientProps);
-    let container: IFluidContainer;
-    let containerId: string;
-
     const div = document.getElementById("content") as HTMLDivElement;
     ReactDOM.render(
         <div className="d-flex justify-content-center m-5">
@@ -36,54 +16,7 @@ async function start(): Promise<void> {
         div,
     );
 
-    // Get or create the document depending if we are running through the create new flow
-    const createNew = location.hash.length === 0;
-    if (createNew) {
-        // The client will create a new detached container using the schema
-        // A detached container will enable the app to modify the container before attaching it to the client
-        ({ container } = await client.createContainer(containerSchema));
-
-        // If the app is in a `createNew` state, and the container is detached, we attach the container.
-        // This uploads the container to the service and connects to the collaboration session.
-        containerId = await container.attach();
-
-        // The newly attached container is given a unique ID that can be used to access the container in another session
-        location.hash = containerId;
-    } else {
-        containerId = location.hash.substring(1);
-        // Use the unique container ID to fetch the container created earlier.  It will already be connected to the
-        // collaboration session.
-        ({ container } = await client.getContainer(
-            containerId,
-            containerSchema,
-        ));
-    }
-
-    const containerInfo = {
-        id: containerId,
-        connected: true,
-        dirty: false,
-    };
-
-    const eventItems = [
-        {
-            id: "1",
-            type: EventItemType.IncommingOp,
-            title: "Incoming op 1",
-            data: { someData1: "hey" },
-        },
-        {
-            id: "2",
-            type: EventItemType.OutgoingOp,
-            title: "Outgoing op 1",
-            data: { someData2: "hey there" },
-        },
-    ];
-
-    ReactDOM.render(
-        <AppView containerInfo={containerInfo} eventItems={eventItems} />,
-        div,
-    );
+    ReactDOM.render(<AppView title={"test"} />, div);
 }
 
 start().catch((error) => console.error(error));
--- examples/apps/container-debugger/src/appView.tsx
+++ examples/apps/container-debugger/src/appView.tsx
@@ -2,28 +2,58 @@
  * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
  * Licensed under the MIT License.
  */
-
+//
+// import { ITelemetryBaseEvent } from "@fluidframework/common-definitions";
 import React, { useEffect, useState } from "react";
-import { Stack } from "react-bootstrap";
 import ReactJson from "react-json-view";
-import { ContainerInfo, EventItem } from "./dataTypes";
+import { EventItem, EventItemType } from "./dataTypes";
 import { EventItemView } from "./eventItemView";
 
 export interface IAppViewProps {
-    containerInfo: ContainerInfo;
-    eventItems?: EventItem[];
+    title: string;
 }
 
 export const AppView: React.FC<IAppViewProps> = (props: IAppViewProps) => {
-    const { containerInfo, eventItems } = props;
+    const [eventItems, setEventItems] = useState<EventItem[]>([]);
+    const [filteredEventItems, setFilteredEventItems] =
+        useState<EventItem[]>(eventItems);
     const [selectedEvent, setSelectedEvent] = useState<EventItem | undefined>(
-        eventItems && eventItems[0],
+        eventItems[0]
     );
+    const [liveContainers, setLiveContainers] = useState<string[]>([]);
+    const [selectedContainer, setSelectedContainer] = useState<
+        string | undefined
+    >(undefined);
 
     useEffect(() => {
         const messageEvent = (event) => {
-            console.log("event--->", event);
-            if (event.origin !== "http://example.org:8080") {return;}
+            let data: any;
+            try {
+                data = JSON.parse(event.data);
+            } catch {
+                console.log("cannot parse event", event);
+                return;
+            }
+
+            const id = Math.round(event.timeStamp * 1000).toString();
+            const newItem = {
+                id,
+                type: EventItemType.IncommingOp,
+                title: `${data.eventName} ${data.containerId}`,
+                data,
+            };
+
+            eventItems.push(newItem);
+            setEventItems([...eventItems]);
+
+            if (liveContainers.indexOf(data.containerId) === -1) {
+                liveContainers.push(data.containerId);
+                setLiveContainers([...liveContainers]);
+            }
+
+            if (event.origin !== "http://example.org:8080") {
+                return;
+            }
             // TODO: Set local state
         };
         window.addEventListener("message", messageEvent);
@@ -32,31 +62,57 @@ export const AppView: React.FC<IAppViewProps> = (props: IAppViewProps) => {
         };
     }, []);
 
+    useEffect(() => {
+        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+        if (selectedContainer) {
+            const items = eventItems.filter(
+                (a) => a.data.containerId === selectedContainer
+            );
+            setFilteredEventItems(items);
+        } else {
+            setFilteredEventItems(eventItems);
+        }
+    }, [selectedContainer]);
+
     return (
-        <div className="container">
-            <Stack gap={5}>
-                <div className="m-5">
-                    <h5>My Container:</h5>
-                    <div style={{ marginBottom: 10, fontSize: 40 }}>
-                        {containerInfo.id}
-                    </div>
+        <div className="container" style={{ marginTop: 40, marginBottom: 120 }}>
+            <div className="row" style={{ marginTop: 40, marginBottom: 40 }}>
+                <div className="col">
+                    <h5>Select Container</h5>
+                    <select
+                        value={selectedContainer ?? ""}
+                        className="form-select"
+                        onChange={(e) => setSelectedContainer(e.target.value)}
+                        aria-label="Default select example"
+                    >
+                        <option
+                            value={undefined}
+                        >
+                            All Containers
+                        </option>
+                        {liveContainers.map((c) => (
+                            <option
+                                key={c}
+                                value={c}
+
+                            >
+                                {c}
+                            </option>
+                        ))}
+                    </select>
                 </div>
-            </Stack>
+                <div className="col"></div>
+            </div>
 
             <div className="row">
                 <div className="col">
-                    <table className="table table-striped table-bordered">
-                        <tbody>
-                            {eventItems &&
-                                eventItems.map((item) => (
-                                    <EventItemView
-                                        key={item.id}
-                                        item={item}
-                                        onSelect={() => setSelectedEvent(item)}
-                                    />
-                                ))}
-                        </tbody>
-                    </table>
+                    {filteredEventItems.map((item) => (
+                        <EventItemView
+                            key={item.id}
+                            item={item}
+                            onSelect={() => setSelectedEvent(item)}
+                        />
+                    ))}
                 </div>
                 <div className="col">
                     <ReactJson
