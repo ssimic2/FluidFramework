90c837f68e2652a8834db20e1ed727874f4c228e
zhenmichael <112977307+zhenmichael@users.noreply.github.com>
2022-09-22 11:22:22 -0700

    upload json modal

--- azure/packages/fhl22/test-app-runner/src/components/HeaderButtons/HeaderButtons.tsx
+++ azure/packages/fhl22/test-app-runner/src/components/HeaderButtons/HeaderButtons.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Button from '@mui/material/Button';
+import { Button } from '@mui/material';
+import StageLoadModal from "../StageLoadModal/StageLoadModal";
 
 function FileCollect() {
     return (
         <div>
             <Button variant="contained">Start</Button>
             <Button variant="contained">Stop</Button>
-            <Button variant="contained">Load</Button>
+            <StageLoadModal/>
         </div>
     )
 }
---
+++ azure/packages/fhl22/test-app-runner/src/components/StageLoadModal/StageLoadModal.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools";
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 0.5,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+export default function StageLoadModal() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    let input: any;
+    const onChange = (newValue: any) => {
+        input = newValue;
+    }
+
+    const handleSubmit = () => {
+        console.log("INPUT SUBMITTED: ", input);
+        handleClose();
+    }
+
+    return (
+      <>
+        <Button variant="contained" onClick={handleOpen}>Load Yaml</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <AceEditor
+                mode="json"
+                theme="github"
+                name="UNIQUE_ID_OF_DIV"
+                onChange={onChange}
+                editorProps={{ $blockScrolling: true }}
+                focus={ true }
+            />
+            <Button variant="contained" onClick={handleSubmit}>Confirm</Button>
+          </Box>
+        </Modal>
+      </>
+    );
+  }
